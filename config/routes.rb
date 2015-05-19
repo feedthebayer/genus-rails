07318151d440e2c520c6c44bac3144b12358e84a
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-05-18 19:44:39 -0500

    feat: add user model and controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   resources :organizations, path: "", only: [:show] do
+    resources :users, only: [:create, :show, :update, :destroy]
   end
 
   root 'static_pages#home'
