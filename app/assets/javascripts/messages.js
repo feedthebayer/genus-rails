14d34957512fd54d64e637f444035c5a44de8885
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-05-30 15:00:46 -0500

    refactor: use $(document).on for js hooks

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,8 +1,17 @@
 $(document).on("page:change page:receive", function() {
   $('.msg-new-submit').hide();
-
-  $('.msg-new-textarea').focus(function() { 
-    $('.msg-new-submit').fadeIn(100); 
-  });
 });
+
+$(document).on('focus', '.msg-new-textarea', function() { 
+  $('.msg-new-submit').fadeIn(100); 
+});
+
+$(document).on('keydown', '.msg-new-textarea', function(e) {
+  // Enter was pressed with shift key
+  if (e.keyCode == 13 && e.shiftKey) {
+    $('.msg-new-submit').click();
+    e.preventDefault();
+  }
+});
+
 
