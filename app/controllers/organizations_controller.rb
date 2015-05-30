aff2b5054e0ed3be0730e3221e316b6c444e57a0
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-05-30 16:20:52 -0500

    refactor: message loading & form partial

--- app/controllers/organizations_controller.rb
+++ app/controllers/organizations_controller.rb
@@ -1,6 +1,6 @@
 class OrganizationsController < ApplicationController
   def show
-    @org = Organization.find(params[:id])
+    @org = Organization.includes(:messages).find(params[:id])
     @messages = @org.messages # TODO - only get today's messages
     @new_message = Message.new(messageable: @org)
   end
