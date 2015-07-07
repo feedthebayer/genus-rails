5cc0cc62f36d3a4b775732aca86bee981ced2366
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-07-07 18:46:35 -0500

    feat(ui): first step at UI 2 - mobile styling only

--- app/assets/javascripts/messages.js
+++ app/assets/javascripts/messages.js
@@ -1,5 +1,10 @@
 $(document).on("page:change page:receive", function() {
+  $('.msg-new-submit').hide();
   $('.msg-reply-submit').hide();
+});
+
+$(document).on('focus', '.msg-new-textarea', function() {
+  $(this).next('.msg-new-submit').fadeIn(150);
 });
 
 $(document).on('focus', '.msg-reply-textarea', function() {
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,9 +1,17 @@
 module ApplicationHelper
-  def log_in_out_link
+  def log_in_out_link_top
     if logged_in?
-      link_to "Log Out", logout_path, method: :delete
+      link_to "Log Out", logout_path, method: :delete, class: "nav-logoutLink-top"
     else
-      link_to "Log In", login_path
+      link_to "Log In", login_path, class: "nav-logoutLink-top"
+    end
+  end
+
+  def log_in_out_link_bottom
+    if logged_in?
+      link_to "Log Out", logout_path, method: :delete, class: "nav-logoutLink-bottom"
+    else
+      link_to "Log In", login_path, class: "nav-logoutLink-bottom"
     end
   end
 end
