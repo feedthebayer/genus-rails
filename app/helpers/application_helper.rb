e4dcee1ec23c6a0ad11daad0124dfe82fd53c3fc
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-07-07 20:52:33 -0500

    feat(ui): add new positioning for medium and up

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,9 +9,9 @@ module ApplicationHelper
 
   def log_in_out_link_bottom
     if logged_in?
-      link_to "Log Out", logout_path, method: :delete, class: "nav-logoutLink-bottom"
+      link_to "Log Out", logout_path, method: :delete, class: "nbs-sessionLink"
     else
-      link_to "Log In", login_path, class: "nav-logoutLink-bottom"
+      link_to "Log In", login_path, class: "nbs-sessionLink"
     end
   end
 end
