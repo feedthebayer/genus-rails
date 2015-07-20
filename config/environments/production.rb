fa0a32470b53d99df059cfd297bc290486e04e79
Brandon Bayer <feedthebayer@users.noreply.github.com>
2015-07-20 18:59:35 -0500

    fix: use https for mailer urls

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,7 +64,7 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   host = 'genusapp.com'
-  config.action_mailer.default_url_options = { host: host }
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
   config.action_mailer.delivery_method = :smtp
   ActionMailer::Base.smtp_settings = {
     :address        => 'smtp.mandrillapp.com',
